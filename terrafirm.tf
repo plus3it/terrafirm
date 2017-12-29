20492281b2c1fe57319c84b350a6007ba0dd3dc3
YakDriver <31492422+YakDriver@users.noreply.github.com>
2017-12-28 20:07:25 -0500

    Update terrafirm.tf

--- terrafirm.tf
+++ terrafirm.tf
@@ -12,8 +12,8 @@ resource "aws_security_group" "terrafirm" {
 
   # SSH access from anywhere
   ingress {
-    from_port   = 5986
-    to_port     = 5986
+    from_port   = 5985
+    to_port     = 5985
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
