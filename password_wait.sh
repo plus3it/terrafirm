151dfe5a7d8143d279f50bf8ae4afed4646211e2
YakDriver <31492422+YakDriver@users.noreply.github.com>
2017-12-29 16:47:27 -0500

    Update password_wait.sh

--- password_wait.sh
+++ password_wait.sh
@@ -9,8 +9,8 @@ chmod 600 private_key
 wait_seconds=90 #waits 10 seconds per so x*10 seconds; 90 = 900s = 15m
 
 until test $((wait_seconds--)) -eq 0 -o -n "$DEC_PASSWORD" ; do #waits for timeout or password to not be empty
-  PASSWORD_DATA="$(aws ec2 get-password-data --instance-id $AWS_INSTANCE_ID)"
-  ENC_PASSWORD="$(echo $PASSWORD_DATA | ./jq.dms -r '."PasswordData"')"
+  PASSWORD_DATA="$(aws ec2 get-password-data --instance-id "$AWS_INSTANCE_ID")"
+  ENC_PASSWORD="$(echo "$PASSWORD_DATA" | ./jq.dms -r '."PasswordData"')"
   sleep 10
 done
 
