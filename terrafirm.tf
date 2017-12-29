ee44d5f7b5111217cc6cb07145e6d5c007381172
YakDriver <31492422+YakDriver@users.noreply.github.com>
2017-12-28 19:21:54 -0500

    Update terrafirm.tf

--- terrafirm.tf
+++ terrafirm.tf
@@ -29,12 +29,9 @@ resource "aws_security_group" "terrafirm" {
 
 resource "aws_instance" "windows" {
   ami = "${var.ami}"
-  #ami = "ami-658ce61f" #spel-minimal-centos-6.9-hvm-2017.12.1.x86_64-gp2
-  #ami = "ami-55ef662f"  #amazon linux 2017.09.1
   instance_type = "t2.micro"
   key_name = "${aws_key_pair.auth.id}"
   vpc_security_group_ids = ["${aws_security_group.terrafirm.id}"]
-  #subnet_id = "${aws_subnet.default.id}"
   user_data = <<USERDATA
 <powershell>
 $BootstrapUrl = "https://raw.githubusercontent.com/plus3it/watchmaker/master/docs/files/bootstrap/watchmaker-bootstrap.ps1"
@@ -62,7 +59,7 @@ USERDATA
   }
   
   connection {
-    #ssh connection to tier-2 instance
+    #winrm connection to tier-2 instance
     user     = "${var.ssh_user}"
     private_key = "${var.private_key}"
     timeout   = "3m"
@@ -72,9 +69,5 @@ USERDATA
   
   provisioner "remote-exec" {
     script = "watchmaker_test.bat"
-    #inline = [
-    #  "sleep 120",
-    #  "/usr/bin/watchmaker --version",
-    #]
   }
 }
