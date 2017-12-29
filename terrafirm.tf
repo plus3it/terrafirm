1fc0538a211a44d005d264308c9f87492fd42122
YakDriver <31492422+YakDriver@users.noreply.github.com>
2017-12-29 18:51:42 -0500

    Update terrafirm.tf

--- terrafirm.tf
+++ terrafirm.tf
@@ -32,26 +32,7 @@ resource "aws_instance" "windows" {
   instance_type = "t2.micro"
   key_name = "${aws_key_pair.auth.id}"
   vpc_security_group_ids = ["${aws_security_group.terrafirm.id}"]
-  user_data = <<USERDATA
-<powershell>
-$BootstrapUrl = "https://raw.githubusercontent.com/plus3it/watchmaker/master/docs/files/bootstrap/watchmaker-bootstrap.ps1"
-$PythonUrl = "https://www.python.org/ftp/python/3.6.3/python-3.6.3-amd64.exe"
-$PypiUrl = "https://pypi.org/simple"
-
-# Download bootstrap file
-$BootstrapFile = "${Env:Temp}\$(${BootstrapUrl}.split('/')[-1])"
-(New-Object System.Net.WebClient).DownloadFile("$BootstrapUrl", "$BootstrapFile")
-
-# Install python
-& "$BootstrapFile" -PythonUrl "$PythonUrl" -Verbose -ErrorAction Stop
-
-# Install watchmaker
-pip install --index-url="$PypiUrl" --upgrade pip setuptools watchmaker
-
-# Run watchmaker
-watchmaker --log-level debug --log-dir=C:\Watchmaker\Logs
-</powershell>
-USERDATA
+  user_data = "${file("userdata.ps1")}"
   
   timeouts {
     create = "30m"
