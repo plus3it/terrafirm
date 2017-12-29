584ebf8f6196090c486f2069677da66e0bdbbdcb
YakDriver <31492422+YakDriver@users.noreply.github.com>
2017-12-29 18:50:30 -0500

    Create userdata.ps1

---
+++ userdata.ps1
@@ -0,0 +1,18 @@
+<powershell>
+$BootstrapUrl = "https://raw.githubusercontent.com/plus3it/watchmaker/master/docs/files/bootstrap/watchmaker-bootstrap.ps1"
+$PythonUrl = "https://www.python.org/ftp/python/3.6.3/python-3.6.3-amd64.exe"
+$PypiUrl = "https://pypi.org/simple"
+
+# Download bootstrap file
+$BootstrapFile = "${Env:Temp}\$(${BootstrapUrl}.split('/')[-1])"
+(New-Object System.Net.WebClient).DownloadFile("$BootstrapUrl", "$BootstrapFile")
+
+# Install python
+& "$BootstrapFile" -PythonUrl "$PythonUrl" -Verbose -ErrorAction Stop
+
+# Install watchmaker
+pip install --index-url="$PypiUrl" --upgrade pip setuptools watchmaker
+
+# Run watchmaker
+watchmaker --log-level debug --log-dir=C:\Watchmaker\Logs
+</powershell>
