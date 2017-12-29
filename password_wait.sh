4537d982faaac9777163f97033fe8fea5de780e7
YakDriver <31492422+YakDriver@users.noreply.github.com>
2017-12-29 16:42:30 -0500

    Update password_wait.sh

--- password_wait.sh
+++ password_wait.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
 
+#create a private key file
+echo $SSH_KEY > private_key
+chmod 600 private_key
+
+#get encrypted password from AWS/Windows instance
+#loop/sleep until the instance is ready, i.e., can provide a password
 wait_seconds=90 #waits 10 seconds per so x*10 seconds; 90 = 900s = 15m
 
 until test $((wait_seconds--)) -eq 0 -o -n "$DEC_PASSWORD" ; do #waits for timeout or password to not be empty
-  PASSWORD_DATA="$(aws ec2 get-password-data --instance-id $AWS_INSTANCE_ID --priv-launch-key private_key)" ; export PASSWORD_DATA
-  DEC_PASSWORD="$(echo $PASSWORD_DATA | ./jq.dms -r '."PasswordData"')" ; export DEC_PASSWORD
+  PASSWORD_DATA="$(aws ec2 get-password-data --instance-id $AWS_INSTANCE_ID)"
+  ENC_PASSWORD="$(echo $PASSWORD_DATA | ./jq.dms -r '."PasswordData"')"
   sleep 10
 done
+
+#Windows or AWS or someone is padding encrypted password with a Windows newline
+#Get rid of this newline for decryption to work
+if [ "${ENC_PASSWORD:0:4}" = "\r\n"]; then
+  ENC_PASSWORD="${ENC_PASSWORD:4}"
+fi
+
+if [ "${ENC_PASSWORD:(-4)}" = "\r\n"]; then
+  ENC_PASSWORD="${ENC_PASSWORD:0:${#ENC_PASSWORD}-4}"
+fi
+
+#Decrypt the password with the private key
+echo ENC_PASSWORD > encryptedpassword
+base64 -D -i encryptedpassword | openssl rsautl -decrypt -inkey private_key -out decryptedpassword
+
+DEC_PASSWORD="$(cat decryptedpassword)" ; export DEC_PASSWORD
+
+#Get rid of temp files
+rm encryptedpassword
+rm decryptedpassword
+rm private_key
