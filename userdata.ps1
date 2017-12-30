166a19bcfdd376f5abe7db37201e15d68ffeaffb
YakDriver <31492422+YakDriver@users.noreply.github.com>
2017-12-29 19:57:50 -0500

    Update userdata.ps1

--- userdata.ps1
+++ userdata.ps1
@@ -2,7 +2,8 @@
 #Needed to allow WinRM 
 #Enable-PSRemoting -Force
 #winrm set winrm/config/service '@{AllowUnencrypted="true"}'
-#winrm set winrm/config/service/auth '@{Basic="true"}'
+#winrm set winrm/config/service/auth '@{Basic="true"}' <----- PROBLEM CHILD
+#https://pubs.vmware.com/orchestrator-plugins/index.jsp?topic=%2Fcom.vmware.using.powershell.plugin.doc_10%2FGUID-D4ACA4EF-D018-448A-866A-DECDDA5CC3C1.html
 
 $BootstrapUrl = "https://raw.githubusercontent.com/plus3it/watchmaker/master/docs/files/bootstrap/watchmaker-bootstrap.ps1"
 $PythonUrl = "https://www.python.org/ftp/python/3.6.3/python-3.6.3-amd64.exe"
