bbd7bef1fe7b9af377d8a44257eb1ba98c684f9f
YakDriver <31492422+YakDriver@users.noreply.github.com>
2017-12-29 19:27:13 -0500

    Update userdata.ps1

--- userdata.ps1
+++ userdata.ps1
@@ -1,8 +1,8 @@
 <powershell>
 #Needed to allow WinRM 
-Enable-PSRemoting -Force
-winrm set winrm/config/service '@{AllowUnencrypted="true"}'
-winrm set winrm/config/service/auth '@{Basic="true"}'
+#Enable-PSRemoting -Force
+#winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+#winrm set winrm/config/service/auth '@{Basic="true"}'
 
 $BootstrapUrl = "https://raw.githubusercontent.com/plus3it/watchmaker/master/docs/files/bootstrap/watchmaker-bootstrap.ps1"
 $PythonUrl = "https://www.python.org/ftp/python/3.6.3/python-3.6.3-amd64.exe"
