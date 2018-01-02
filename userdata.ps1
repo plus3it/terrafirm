258219db2e2000e3338ed70be2ade4cb22d98b82
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-02 11:37:17 -0500

    Update userdata.ps1

--- userdata.ps1
+++ userdata.ps1
@@ -1,5 +1,17 @@
+<script>
+  winrm quickconfig -q & winrm set winrm/config @{MaxTimeoutms="1800000"} & winrm set winrm/config/service @{AllowUnencrypted="true"} & winrm set winrm/config/service/auth @{Basic="true"}
+</script>
 <powershell>
+# Get ready for winrm for terraform provisioner connection
 
+# open firewall for winrm
+netsh advfirewall firewall add rule name="WinRM in" protocol=TCP dir=in profile=any localport=5985 remoteip=any localip=any action=allow
+
+# Set Administrator password
+$admin = [adsi]("WinNT://./administrator, user")
+$admin.psbase.invoke("SetPassword", "${var.admin_password}")
+
+# Setup watchmaker
 $BootstrapUrl = "https://raw.githubusercontent.com/plus3it/watchmaker/master/docs/files/bootstrap/watchmaker-bootstrap.ps1"
 $PythonUrl = "https://www.python.org/ftp/python/3.6.3/python-3.6.3-amd64.exe"
 $PypiUrl = "https://pypi.org/simple"
@@ -18,12 +30,12 @@ pip install --index-url="$PypiUrl" --upgrade pip setuptools watchmaker
 watchmaker -n --log-level debug --log-dir=C:\Watchmaker\Logs
 
 #Needed to allow WinRM 
-Start-Sleep -s 30
-winrm set winrm/config/service '@{AllowUnencrypted="true"}'
-Start-Sleep -s 10
-Enable-PSRemoting -Force
-Start-Sleep -s 10
-winrm set winrm/config/service/auth '@{Basic="true"}'
+#Start-Sleep -s 30
+#winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+#Start-Sleep -s 10
+#Enable-PSRemoting -Force
+#Start-Sleep -s 10
+#winrm set winrm/config/service/auth '@{Basic="true"}'
 #https://pubs.vmware.com/orchestrator-plugins/index.jsp?topic=%2Fcom.vmware.using.powershell.plugin.doc_10%2FGUID-D4ACA4EF-D018-448A-866A-DECDDA5CC3C1.html
 
 </powershell>
