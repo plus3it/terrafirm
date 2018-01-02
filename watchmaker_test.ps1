138bae7bb47cd48a02e47651e4c665ccd422396a
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-02 17:22:34 -0500

    Update watchmaker_test.ps1

--- watchmaker_test.ps1
+++ watchmaker_test.ps1
@@ -1,3 +1,35 @@
-<powershell>
-watchmaker --version 
-</powershell>
+function Retry-Command
+{
+    param (
+    [Parameter(Mandatory=$true)][string]$command, 
+    [Parameter(Mandatory=$true)][hashtable]$args, 
+    [Parameter(Mandatory=$false)][int]$retries = 5, 
+    [Parameter(Mandatory=$false)][int]$secondsDelay = 2
+    )
+    
+    # Setting ErrorAction to Stop is important. This ensures any errors that occur in the command are 
+    # treated as terminating errors, and will be caught by the catch block.
+    $args.ErrorAction = "Stop"
+    
+    $retrycount = 0
+    $completed = $false
+
+    while (-not $completed) {
+        try {
+            & $command @args
+            Write-Verbose ("Command [{0}] succeeded." -f $command)
+            $completed = $true
+        } catch {
+            if ($retrycount -ge $retries) {
+                Write-Verbose ("Command [{0}] failed the maximum number of {1} times." -f $command, $retrycount)
+                throw
+            } else {
+                Write-Verbose ("Command [{0}] failed. Retrying in {1} seconds." -f $command, $secondsDelay)
+                Start-Sleep $secondsDelay
+                $retrycount++
+            }
+        }
+    }
+}
+
+Retry-Command -Command 'watchmaker' -Args '--version' -Verbose
