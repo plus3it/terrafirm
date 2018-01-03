f30cf808bb9077387fab97d0e419976b7ca1dbe7
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-02 19:34:27 -0500

    Update watchmaker_test.ps1

--- watchmaker_test.ps1
+++ watchmaker_test.ps1
@@ -1,40 +1,25 @@
-$Logfile = "terrafirm_win.log"
-
-Function LogWrite
-{
-   Param ([string]$logstring)
-
-   Add-content $Logfile -value $logstring
-}
-
 function Retry-Command
 {
     param (
-    [Parameter(Mandatory=$true)][string]$command, 
-    [Parameter(Mandatory=$true)][hashtable]$args, 
-    [Parameter(Mandatory=$false)][int]$retries = 5, 
-    [Parameter(Mandatory=$false)][int]$secondsDelay = 2
+     [Parameter(Mandatory=$true)][string]$command,
+     [Parameter(Mandatory=$false)][int]$retries = 5,
+     [Parameter(Mandatory=$false)][int]$secondsDelay = 2
     )
-    
-    # Setting ErrorAction to Stop is important. This ensures any errors that occur in the command are 
-    # treated as terminating errors, and will be caught by the catch block.
-    $args.ErrorAction = "Stop"
-    
+
     $retrycount = 0
     $completed = $false
 
     while (-not $completed) {
         try {
-            .$profile
-            & $command @args
-            LogWrite ("Command [{0}] succeeded." -f $command)
+            Invoke-Expression -Command:$command
+            Write-Host ("Command [{0}] succeeded." -f $command)
             $completed = $true
         } catch {
             if ($retrycount -ge $retries) {
-                LogWrite ("Command [{0}] failed the maximum number of {1} times." -f $command, $retrycount)
+                Write-Host ("Command [{0}] failed the maximum number of {1} times." -f $command, $retrycount)
                 throw
             } else {
-                LogWrite ("Command [{0}] failed. Retrying in {1} seconds." -f $command, $secondsDelay)
+                Write-Host ("Command [{0}] failed. Retrying in {1} seconds." -f $command, $secondsDelay)
                 Start-Sleep $secondsDelay
                 $retrycount++
             }
@@ -42,4 +27,4 @@ function Retry-Command
     }
 }
 
-Retry-Command -Command 'watchmaker --version' -Retries 10 -SecondsDelay 5
+Retry-Command -Command 'watchmaker --version' -Retries 100 -SecondsDelay 5
