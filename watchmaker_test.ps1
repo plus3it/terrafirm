79ce9f1b41566a0ae6ceb1db516bfc7247257d82
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-02 20:11:55 -0500

    Update watchmaker_test.ps1

--- watchmaker_test.ps1
+++ watchmaker_test.ps1
@@ -27,4 +27,4 @@ function Retry-Command
     }
 }
 
-Retry-Command -Command 'watchmaker --version' -Retries 100 -SecondsDelay 5
+Retry-Command -Command 'watchmaker --version' -Retries 20 -SecondsDelay 30
