284ada0fd596a1c6a5b3413c324dd61add609c6b
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-04 12:16:14 -0500

    Update and rename test.ps1 to manage_tests.ps1

--- manage_tests.ps1
+++ manage_tests.ps1
@@ -1,3 +1,5 @@
 Invoke-Expression -Command:"powershell.exe -File C:\scripts\watchmaker_test.ps1"
+Start-Sleep 30
 Invoke-Expression -Command:"powershell.exe -File C:\scripts\watchmaker_test.ps1"
+Start-Sleep 30
 Invoke-Expression -Command:"powershell.exe -File C:\scripts\watchmaker_test.ps1"
