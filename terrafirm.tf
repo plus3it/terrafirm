2075e94164803d3ce50b99ad826bd8816e793fa3
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-04 17:49:52 -0500

    Update terrafirm.tf

--- terrafirm.tf
+++ terrafirm.tf
@@ -73,7 +73,7 @@ data "aws_ami" "windows2012" {
   
   filter {
     name = "name"
-    values = ["Windows_Server-2012*"]
+    values = ["Windows_Server-2012-R2*English*Base"]
   }
   
   #filter {
@@ -100,7 +100,7 @@ data "aws_ami" "windows2008" {
   
   filter {
     name = "name"
-    values = ["Windows_Server-2008*"]
+    values = ["Windows_Server-2008-R2_SP2-English*Base*"]
   }
   
   #filter {
