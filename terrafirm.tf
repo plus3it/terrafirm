80c66975e8592ce0479137e55d1ad13021b48486
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-04 18:05:54 -0500

    Update terrafirm.tf

--- terrafirm.tf
+++ terrafirm.tf
@@ -54,7 +54,7 @@ data "aws_ami" "windows2016" {
   #  values = ["099720109477","801119661308"]
   #}
   
-  owners = ["099720109477","801119661308"]
+  owners = ["099720109477","801119661308","amazon"]
   
 }
 
@@ -82,7 +82,7 @@ data "aws_ami" "windows2012" {
   #}
   
   name_regex = "R2.*English.*Base"
-  owners = ["099720109477","801119661308"]
+  owners = ["099720109477","801119661308","amazon"]
   
 }
 
@@ -110,7 +110,7 @@ data "aws_ami" "windows2008" {
   #}
   
   name_regex = "2008.*R2.*English.*Base"
-  owners = ["099720109477","801119661308"]
+  owners = ["099720109477","801119661308","amazon"]
   
 }
 
