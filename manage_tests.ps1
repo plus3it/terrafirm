b8f01d3050e0835f26bcd99fe0d9d22342c8bb2b
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-04 13:04:30 -0500

    Update manage_tests.ps1

--- manage_tests.ps1
+++ manage_tests.ps1
@@ -3,3 +3,4 @@ Start-Sleep 30
 Invoke-Expression -Command:"powershell.exe -File C:\scripts\watchmaker_test.ps1"
 Start-Sleep 30
 Invoke-Expression -Command:"powershell.exe -File C:\scripts\watchmaker_test.ps1"
+throw
