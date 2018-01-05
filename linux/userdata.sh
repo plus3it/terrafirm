f4a4f4870b25fe3c96b890d44fd0c19a7dd8cc4d
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-04 19:58:38 -0500

    Update userdata.sh

--- linux/userdata.sh
+++ linux/userdata.sh
@@ -1,1 +1,12 @@
+#!/bin/sh
+PIP_URL=https://bootstrap.pypa.io/get-pip.py
+PYPI_URL=https://pypi.org/simple
 
+# Install pip
+curl "$PIP_URL" | python - --index-url="$PYPI_URL" wheel==0.29.0
+
+# Install watchmaker
+pip install --index-url="$PYPI_URL" --upgrade pip setuptools watchmaker
+
+# Run watchmaker
+/usr/local/bin/watchmaker -n --log-level debug --log-dir=/var/log/watchmaker
