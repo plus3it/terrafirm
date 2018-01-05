b055e6e115cc1c345ca95cba46edc2519e0870bd
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-04 20:00:07 -0500

    Create watchmaker_test.sh

---
+++ linux/watchmaker_test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "**************************************************************************************************"
+echo "Running Watchmaker test script"
+echo "**************************************************************************************************"
+
+
+wait_file() {
+  local file="$1"; shift
+  local wait_seconds="${1:-10}"; shift # 10 seconds as default timeout
+
+  until test $((wait_seconds--)) -eq 0 -o -f "$file" ; do sleep 5; done
+
+  ((++wait_seconds))
+}
+
+exec_file=/usr/bin/watchmaker
+
+wait_file "$exec_file" 100 || {
+  echo "Executable on remote instance never became available for $? seconds: '$exec_file'"
+  exit 1
+}
+
+/usr/bin/watchmaker --version
