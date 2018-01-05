eb97e3e769035c641503fbff9b56d6f44d64457d
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-04 19:04:15 -0500

    Delete manage_tests.ps1

--- manage_tests.ps1
+++
@@ -1,6 +0,0 @@
-Invoke-Expression -Command:"powershell.exe -File C:\scripts\watchmaker_test.ps1"
-Start-Sleep 30
-Invoke-Expression -Command:"powershell.exe -File C:\scripts\watchmaker_test.ps1"
-Start-Sleep 30
-Invoke-Expression -Command:"powershell.exe -File C:\scripts\watchmaker_test.ps1"
-throw
