e50a65d44836396dbc53739d35c542072b854c09
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-05 14:29:49 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,96 +7,32 @@ variable "ssh_user" {}
 variable "ssh_user_fedora" {}
 variable "cb_ip" {}
 
-output "ipfed" {
-  value = "${aws_instance.fedora.public_ip}"
-}
-
-output "idfedora" {
-  value = "${aws_instance.fedora.id}"
-}
-
 output "amifedora" {
   value = "${data.aws_ami.fedora.id}"
 }
-
-#output "ipcentos6" {
-#  value = "${aws_instance.centos6.public_ip}"
-#}
-
-#output "idcentos6" {
-#  value = "${aws_instance.centos6.id}"
-#}
 
 output "amicentos6" {
   value = "${data.aws_ami.centos6.id}"
 }
 
-#output "ipcentos7" {
-#  value = "${aws_instance.centos7.public_ip}"
-#}
-
-#output "idcentos7" {
-#  value = "${aws_instance.centos7.id}"
-#}
-
 output "amicentos7" {
   value = "${data.aws_ami.centos7.id}"
 }
-
-#output "iprhel6" {
-#  value = "${aws_instance.rhel6.public_ip}"
-#}
-
-#output "idrhel6" {
-#  value = "${aws_instance.rhel6.id}"
-#}
 
 output "amirhel6" {
   value = "${data.aws_ami.rhel6.id}"
 }
 
-#output "iprhel7" {
-#  value = "${aws_instance.rhel7.public_ip}"
-#}
-
-#output "idrhel7" {
-#  value = "${aws_instance.rhel7.id}"
-#}
-
 output "amirhel7" {
   value = "${data.aws_ami.rhel7.id}"
-}
-
-output "ip2016" {
-  value = "${aws_instance.windows2016.public_ip}"
-}
-
-output "id2016" {
-  value = "${aws_instance.windows2016.id}"
 }
 
 output "ami2016" {
   value = "${data.aws_ami.windows2016.id}"
 }
 
-output "ip2012" {
-  value = "${aws_instance.windows2012.public_ip}"
-}
-
-output "id2012" {
-  value = "${aws_instance.windows2012.id}"
-}
-
 output "ami2012" {
   value = "${data.aws_ami.windows2012.id}"
-}
-
-output "ip2008" {
-  value = "${aws_instance.windows2008.public_ip}"
-}
-
-output "id2008" {
-  value = "${aws_instance.windows2008.id}"
 }
 
 output "ami2008" {
