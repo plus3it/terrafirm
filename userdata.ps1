212dc86b503ddec56748f871d4522e741d6ada76
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-04 19:06:52 -0500

    Update userdata.ps1

--- userdata.ps1
+++ userdata.ps1
@@ -28,14 +28,4 @@ pip install --index-url="$PypiUrl" --upgrade pip setuptools watchmaker
 
 # Run watchmaker
 watchmaker -n --log-level debug --log-dir=C:\Watchmaker\Logs
-
-#Needed to allow WinRM 
-#Start-Sleep -s 30
-#winrm set winrm/config/service '@{AllowUnencrypted="true"}'
-#Start-Sleep -s 10
-#Enable-PSRemoting -Force
-#Start-Sleep -s 10
-#winrm set winrm/config/service/auth '@{Basic="true"}'
-#https://pubs.vmware.com/orchestrator-plugins/index.jsp?topic=%2Fcom.vmware.using.powershell.plugin.doc_10%2FGUID-D4ACA4EF-D018-448A-866A-DECDDA5CC3C1.html
-
 </powershell>
