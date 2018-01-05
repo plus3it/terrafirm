2ea74dc7599d97e872f0e35a5a8b07ad3d05289b
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-04 19:10:00 -0500

    Update watchmaker_test.ps1

--- windows/watchmaker_test.ps1
+++ windows/watchmaker_test.ps1
@@ -1,1 +1,32 @@
-aa
+function Retry-Command
+{
+    param (
+     [Parameter(Mandatory=$true)][string]$command,
+     [Parameter(Mandatory=$false)][int]$retries = 5,
+     [Parameter(Mandatory=$false)][int]$secondsDelay = 2
+    )
+
+    $retrycount = 0
+    $success = $false
+
+    while (-not $success) {
+        try {
+            Invoke-Expression -Command:$command
+            Write-Host ("Command [{0}] succeeded." -f $command)
+            $success = $true
+        } catch {
+            if ($retrycount -ge $retries) {
+                Write-Host ("Command [{0}] failed the maximum number of {1} times." -f $command, $retrycount)
+                throw
+            } else {
+                Write-Host ("Command [{0}] failed. Retrying in {1} seconds." -f $command, $secondsDelay)
+                Start-Sleep $secondsDelay
+                $retrycount++
+            }
+        }
+    }
+    
+    return $success
+}
+
+Retry-Command -Command 'watchmaker --version' -Retries 9 -SecondsDelay 30
