7c0cba7ecfc8951d3cb05913461959c00be345b8
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-04 22:50:36 -0500

    Create fedora_userdata.sh

---
+++ linux/fedora_userdata.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+PIP_URL=https://bootstrap.pypa.io/get-pip.py
+PYPI_URL=https://pypi.org/simple
+
+# Install pip
+curl "$PIP_URL" | python - --index-url="$PYPI_URL" wheel==0.29.0
+
+# Fedora
+dnf install -y python-pip
+dnf install -y python3
+
+# Install watchmaker
+pip install --index-url="$PYPI_URL" --upgrade pip setuptools watchmaker
+
+# Run watchmaker
+watchmaker -n --log-level debug --log-dir=/var/log/watchmaker
