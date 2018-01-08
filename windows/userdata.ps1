369a85354bbc9baad76ffd4bd1c162f27c7d0090
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-08 18:09:37 -0500

    Update userdata.ps1

--- windows/userdata.ps1
+++ windows/userdata.ps1
@@ -10,22 +10,36 @@ netsh advfirewall firewall add rule name="WinRM in" protocol=TCP dir=in profile=
 # Set Administrator password
 $admin = [adsi]("WinNT://./administrator, user")
 $admin.psbase.invoke("SetPassword", "THIS_IS_NOT_THE_PASSWORD")
+</powershell>
+<powershell>
+$GitRepo = "https://github.com/plus3it/watchmaker.git"
+$GitBranch = "develop"
 
-# Setup watchmaker
 $BootstrapUrl = "https://raw.githubusercontent.com/plus3it/watchmaker/master/docs/files/bootstrap/watchmaker-bootstrap.ps1"
 $PythonUrl = "https://www.python.org/ftp/python/3.6.3/python-3.6.3-amd64.exe"
+$GitUrl = "https://github.com/git-for-windows/git/releases/download/v2.14.3.windows.1/Git-2.14.3-64-bit.exe"
 $PypiUrl = "https://pypi.org/simple"
 
 # Download bootstrap file
-$BootstrapFile = "${Env:Temp}\$(${BootstrapUrl}.split('/')[-1])"
-(New-Object System.Net.WebClient).DownloadFile("$BootstrapUrl", "$BootstrapFile")
+$BootstrapFile = "${Env:Temp}\$(${BootstrapUrl}.split("/")[-1])"
+(New-Object System.Net.WebClient).DownloadFile($BootstrapUrl, $BootstrapFile)
 
-# Install python
-& "$BootstrapFile" -PythonUrl "$PythonUrl" -Verbose -ErrorAction Stop
+# Install python and git
+& "$BootstrapFile" `
+    -PythonUrl "$PythonUrl" `
+    -GitUrl "$GitUrl" `
+    -Verbose -ErrorAction Stop
+
+# Upgrade pip and setuptools
+pip install --index-url="$PypiUrl" --upgrade pip setuptools
+
+# Clone watchmaker
+git clone "$GitRepo" --branch "$GitBranch" --recursive
 
 # Install watchmaker
-pip install --index-url="$PypiUrl" --upgrade pip setuptools watchmaker
+cd watchmaker
+pip install --index-url "$PypiUrl" --editable .
 
 # Run watchmaker
-watchmaker -n --log-level debug --log-dir=C:\Watchmaker\Logs
+watchmaker --log-level debug --log-dir=C:\Watchmaker\Logs
 </powershell>
