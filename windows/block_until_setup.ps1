5bae75739120f88c51287700d3aa4e7136991d67
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-09 15:24:27 -0500

    Update block_until_setup.ps1

--- windows/block_until_setup.ps1
+++ windows/block_until_setup.ps1
@@ -1,6 +1,6 @@
 #Wait for the signal from the userdata script before testing
 while (!(.\check_block.ps1)) {
   Write-Host ("Setup not complete. Retrying...")
-  Start-Sleep 20 
+  Start-Sleep 10 
 }
 Write-Host ("Setup complete!")
