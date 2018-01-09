3633beb7feca01c9d4d6a75bb5719a52c8dbef02
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-09 15:21:22 -0500

    Update block_until_setup.ps1

--- windows/block_until_setup.ps1
+++ windows/block_until_setup.ps1
@@ -1,20 +1,5 @@
-function Reload-Profile {
-    @(
-        $Profile.AllUsersAllHosts,
-        $Profile.AllUsersCurrentHost,
-        $Profile.CurrentUserAllHosts,
-        $Profile.CurrentUserCurrentHost
-    ) | % {
-        if(Test-Path $_){
-            Write-Verbose "Running $_"
-            . $_
-        }
-    }    
-}
-
 #Wait for the signal from the userdata script before testing
-while (!(Test-Path 'C:\Temp\SETUP_COMPLETE_SIGNAL')) {
-  . Reload-Profile
+while (!(.\check_block.ps1)) {
   Write-Host ("Setup not complete. Retrying...")
   Start-Sleep 20 
 }
