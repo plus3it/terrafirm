f6ba369c004faf2a4ec06d5383392468b737260e
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-08 21:24:58 -0500

    Update userdata.ps1

--- windows/userdata.ps1
+++ windows/userdata.ps1
@@ -44,5 +44,6 @@ pip install --index-url "$PypiUrl" --editable .
 watchmaker -n --log-level debug --log-dir=C:\Watchmaker\Logs
 
 # Signal completion of userdata
-New-Item C://scripts//SIGNAL -type file
+New-Item C:\scripts -type directory -force
+New-Item C:\scripts\SIGNAL.txt -type file -force
 </powershell>
