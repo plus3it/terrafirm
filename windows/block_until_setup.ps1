99782f2b473ecd8820f75077a9c028cdf05137c5
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-09 13:48:13 -0500

    Update block_until_setup.ps1

--- windows/block_until_setup.ps1
+++ windows/block_until_setup.ps1
@@ -1,50 +1,5 @@
-$signal_file = "C:\tmp\SIGNAL.txt"
-$retries = 12
-$seconds_between_retries = 30
-$test_command = 'watchmaker --version'
-
-function Retry-Command
-{
-    param (
-     [Parameter(Mandatory=$true)][string]$command,
-     [Parameter(Mandatory=$false)][int]$retries = 5,
-     [Parameter(Mandatory=$false)][int]$secondsDelay = 2
-    )
-
-    $retrycount = 0
-    $success = $false
-
-    while (-not $success) {
-        try {
-            Invoke-Expression -Command:$command
-            Write-Host ("Command [{0}] succeeded." -f $command)
-            Write-Host (".............................................................................Success!")
-            $success = $true
-        } catch {
-            if ($retrycount -ge $retries) {
-                Write-Host ("Command [{0}] failed the maximum number of {1} times." -f $command, $retrycount)
-                throw
-            } else {
-                Write-Host ("Command [{0}] failed. Retrying in {1} seconds." -f $command, $secondsDelay)
-                Start-Sleep $secondsDelay
-                $retrycount++
-            }
-        }
-    }
-    
-    return $success
-}
-
-Write-Host ("*****************************************************************************")
-Write-Host ("Running Watchmaker test script: WINDOWS")
-Write-Host ("*****************************************************************************")
-
 #Wait for the signal from the userdata script before testing
-#while (!(Test-Path $signal_file)) {
-#    Write-Host ("Waiting for Watchmaker install to complete...")
-#    Start-Sleep 20 
-#}
-
-#Perform test
-Write-Host ("Performing test...")
-Retry-Command -Command 'watchmaker --version' -Retries $retries -SecondsDelay $seconds_between_retries
+while (!(Test-Path 'C:\tmp\SIGNAL')) {
+    Write-Host ("Setup not complete. Retrying...")
+    Start-Sleep 20 
+}
