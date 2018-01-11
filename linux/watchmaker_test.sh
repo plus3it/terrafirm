c40c89c3083234e6f3e672b45823a9e5894785cb
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-11 15:48:50 -0500

    Update watchmaker_test.sh

--- linux/watchmaker_test.sh
+++ linux/watchmaker_test.sh
@@ -4,29 +4,12 @@ echo "**************************************************************************
 echo "Running Watchmaker test script: LINUX"
 echo "*****************************************************************************"
 
-#cat /tmp/userdata_install.txt
+command = "watchmaker --version"
 
-wait_file() {
-  local file="$1"; shift
-  local wait_seconds="${1:-10}"; shift # 10 seconds as default timeout
-
-  until test $((wait_seconds--)) -eq 0 -o -f "$file" ; do
-    echo "Call to watchmaker FAILED. Trying again in 30 seconds..."
-    sleep 30
-  done
-  
-  if [ -f "$file" ]; then
-    echo ".............................................................................Success!"
-  fi
-
-  ((++wait_seconds))
-}
-
-exec_file=/usr/bin/watchmaker
-
-wait_file "$exec_file" 12 || {
-  echo "Executable on remote instance never became available for $? seconds: '$exec_file'"
+if $command ; then
+  echo ".............................................................................Success!"
+  exit 0
+else
+  echo ".............................................................................FAILED!"
   exit 1
-}
-
-watchmaker --version
+fi
