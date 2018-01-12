baa3c0a0be7812a9df4a60e262dfa567d4e42175
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-11 19:51:21 -0500

    Update userdata.ps1

--- windows/userdata.ps1
+++ windows/userdata.ps1
@@ -10,6 +10,8 @@ netsh advfirewall firewall add rule name="WinRM in" protocol=TCP dir=in profile=
 # Set Administrator password
 $admin = [adsi]("WinNT://./administrator, user")
 $admin.psbase.invoke("SetPassword", "THIS_IS_NOT_THE_PASSWORD")
+$admin.description = "State0"
+$admin.psbase.CommitChanges()
 
 #### watchmaker starts here ####
 $GitRepo = "THIS_IS_NOT_THE_REPO"
@@ -32,6 +34,8 @@ $BootstrapFile = "${Env:Temp}\$(${BootstrapUrl}.split("/")[-1])"
 
 # Upgrade pip and setuptools
 pip install --index-url="$PypiUrl" --upgrade pip setuptools boto3
+$admin.description = "State1"
+$admin.psbase.CommitChanges()
 
 # Clone watchmaker
 git clone "$GitRepo" --branch "$GitBranch" --recursive
@@ -39,9 +43,13 @@ git clone "$GitRepo" --branch "$GitBranch" --recursive
 # Install watchmaker
 cd watchmaker
 pip install --index-url "$PypiUrl" --editable .
+$admin.description = "State2"
+$admin.psbase.CommitChanges()
 
 # Run watchmaker
 watchmaker -n --log-level debug --log-dir=C:\Watchmaker\Logs
+$admin.description = "State3"
+$admin.psbase.CommitChanges()
 
 # Signal completion of userdata
 New-Item 'C:\Temp\SETUP_COMPLETE_SIGNAL' -type file -force
