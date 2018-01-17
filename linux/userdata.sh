291c5c3d4ce04693a7941028027017c2d3f39813
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-17 13:21:05 -0500

    Update userdata.sh

--- linux/userdata.sh
+++ linux/userdata.sh
@@ -2,12 +2,15 @@
 
 exec &> /tmp/watchmaker_userdata_install.log
 
-/sbin/iptables -A INPUT -p tcp --destination-port 2222 -j DROP
-/sbin/service iptables save
-
 WATCHMAKER_INSTALL_GOES_HERE
 
-sed -i '5iPort 2222' /etc/ssh/sshd_config
-/sbin/service sshd restart
-/sbin/iptables -A INPUT -p tcp --destination-port 2222 -j ACCEPT
+#sed -i '5iPort 2222' /etc/ssh/sshd_config
+
+  # Change ssh port. Set packer to connect to port 122; this way packer waits
+  # until this script is complete.
+sed -i -e "s/^[#]*Port .*$/Port 122/" /etc/ssh/sshd_config
+service sshd restart
+  
+#/sbin/service sshd restart
+/sbin/iptables -A INPUT -p tcp --destination-port 122 -j ACCEPT
 /sbin/service iptables save
