266dfb66afd818f7c61200c9f81930512f55c27d
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-17 09:18:39 -0500

    Create install_watchmaker.ps1

---
+++ windows/install_watchmaker.ps1
@@ -0,0 +1,30 @@
+$GitRepo = "THIS_IS_NOT_THE_REPO"
+$GitBranch = "THIS_IS_NOT_THE_BRANCH"
+
+$BootstrapUrl = "https://raw.githubusercontent.com/plus3it/watchmaker/master/docs/files/bootstrap/watchmaker-bootstrap.ps1"
+$PythonUrl = "https://www.python.org/ftp/python/3.6.3/python-3.6.3-amd64.exe"
+$GitUrl = "https://github.com/git-for-windows/git/releases/download/v2.14.3.windows.1/Git-2.14.3-64-bit.exe"
+$PypiUrl = "https://pypi.org/simple"
+
+# Download bootstrap file
+$BootstrapFile = "${Env:Temp}\$(${BootstrapUrl}.split("/")[-1])"
+(New-Object System.Net.WebClient).DownloadFile($BootstrapUrl, $BootstrapFile)
+
+# Install python and git
+& "$BootstrapFile" `
+    -PythonUrl "$PythonUrl" `
+    -GitUrl "$GitUrl" `
+    -Verbose -ErrorAction Stop
+
+# Upgrade pip and setuptools
+pip install --index-url="$PypiUrl" --upgrade pip setuptools boto3
+
+# Clone watchmaker
+git clone "$GitRepo" --branch "$GitBranch" --recursive
+
+# Install watchmaker
+cd watchmaker
+pip install --index-url "$PypiUrl" --editable .
+
+# Run watchmaker
+watchmaker -n --log-level debug --log-dir=C:\Watchmaker\Logs
