f8bbeeb14a72ac0d6f916f06165b6e93d8311246
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-17 09:17:01 -0500

    Update userdata.ps1

--- windows/userdata.ps1
+++ windows/userdata.ps1
@@ -2,6 +2,8 @@
 
 Start-Transcript -path C:\scripts\watchmaker_install.txt -append
 
+netsh advfirewall firewall add rule name="WinRM in" protocol=TCP dir=in profile=any localport=5985 remoteip=any localip=any action=deny
+
 #$admin.description = "Stage0"
 #$admin.psbase.CommitChanges()
 
