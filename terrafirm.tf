8d53533fc364e0faa26490c2fa4db9615161a6ef
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-17 14:38:24 -0500

    Update terrafirm.tf

--- terrafirm.tf
+++ terrafirm.tf
@@ -219,8 +219,8 @@ data "null_data_source" "spel_instance_amis" {
 
 # bread & butter - this tells TF the provision/create the actual instance
 resource "aws_instance" "spels" {
-  count = "1"
-  #count = "${length(data.null_data_source.spel_instance_amis.inputs)}"
+  #count = "1"
+  count = "${length(data.null_data_source.spel_instance_amis.inputs)}"
   ami = "${lookup(data.null_data_source.spel_instance_amis.inputs, count.index)}"
   instance_type = "t2.micro"
   key_name = "${aws_key_pair.auth.id}"
