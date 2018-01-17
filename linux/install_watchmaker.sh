a612e381d6be121a192dddbeb5bdb542e27e191c
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-17 10:28:12 -0500

    Update install_watchmaker.sh

--- linux/install_watchmaker.sh
+++ linux/install_watchmaker.sh
@@ -1,1 +1,24 @@
+GIT_REPO=THIS_IS_NOT_THE_REPO
+GIT_BRANCH=THIS_IS_NOT_THE_BRANCH
 
+PIP_URL=https://bootstrap.pypa.io/get-pip.py
+PYPI_URL=https://pypi.org/simple
+
+# Install pip
+curl "$PIP_URL" | python - --index-url="$PYPI_URL" wheel==0.29.0
+
+# Install git
+yum -y install git
+
+# Upgrade pip and setuptools
+pip install --index-url="$PYPI_URL" --upgrade pip setuptools boto3
+
+# Clone watchmaker
+git clone "$GIT_REPO" --branch "$GIT_BRANCH" --recursive
+
+# Install watchmaker
+cd watchmaker
+pip install --index-url "$PYPI_URL" --editable .
+
+# Run watchmaker
+watchmaker -n --log-level debug --log-dir=/var/log/watchmaker
