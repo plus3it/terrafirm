5e1ec9f7dbe610f5a0dee8d2497dee618f7cea75
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-17 14:22:48 -0500

    Update userdata.sh

--- linux/userdata.sh
+++ linux/userdata.sh
@@ -4,10 +4,4 @@ exec &> /tmp/watchmaker_userdata_install.log
 
 WATCHMAKER_INSTALL_GOES_HERE
 
-#sed -i -e "s/^[#]*Port .*$/Port 122/" /etc/ssh/sshd_config
-sed -i '5iPort 122' /etc/ssh/sshd_config
-service sshd restart
-  
-#/sbin/service sshd restart
-#iptables -I INPUT -p tcp -m tcp --dport 122 -j ACCEPT
-#service iptables save
+touch /tmp/SETUP_COMPLETE_SIGNAL
