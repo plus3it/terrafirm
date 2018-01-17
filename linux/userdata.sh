8a53cacecde21902529285a128bcc28011ba6440
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-17 11:37:21 -0500

    Update userdata.sh

--- linux/userdata.sh
+++ linux/userdata.sh
@@ -2,10 +2,10 @@
 
 exec &> /tmp/watchmaker_userdata_install.log
 
-iptables -A INPUT -p tcp --destination-port 22 -j DROP
+iptables -A INPUT -p tcp --destination-port 122 -j DROP
 service iptables save
 
 WATCHMAKER_INSTALL_GOES_HERE
 
-iptables -A INPUT -p tcp --destination-port 22 -j ACCEPT
+iptables -A INPUT -p tcp --destination-port 122 -j ACCEPT
 service iptables save
