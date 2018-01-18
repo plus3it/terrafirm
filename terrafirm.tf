e7dc68ed76324917a9d8c44d92aa271f04771cc0
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-18 16:31:18 -0500

    Update terrafirm.tf

--- terrafirm.tf
+++ terrafirm.tf
@@ -211,11 +211,11 @@ data "null_data_source" "spel_instance_amis" {
 
 # bread & butter - this tells TF the provision/create the actual instance
 resource "aws_instance" "spels" {
-  count                        = "0"
-  #count                       = "${length(data.null_data_source.spel_instance_amis.inputs)}"
+  #count                        = "0"
+  count                       = "${length(data.null_data_source.spel_instance_amis.inputs)}"
   ami                          = "${lookup(data.null_data_source.spel_instance_amis.inputs, count.index)}"
   instance_type                = "${var.lx_instance_type}"
-  iam_instance_profile         = "${var.instance_profile}"
+  #iam_instance_profile         = "${var.instance_profile}"
   key_name                     = "${aws_key_pair.auth.id}"
   vpc_security_group_ids       = ["${aws_security_group.terrafirm_ssh.id}"]
   user_data                    = "${file("linux/userdata.sh")}"
