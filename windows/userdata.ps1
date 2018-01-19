3248a5d1597fb8178c8a71e146c73e842f2a216b
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-19 14:24:43 -0500

    Update userdata.ps1

--- windows/userdata.ps1
+++ windows/userdata.ps1
@@ -3,15 +3,21 @@
 # log of the userdata install
 Start-Transcript -path C:\Temp\watchmaker_userdata_install.log -append
 
-# close the firewall
-netsh advfirewall firewall add rule name="WinRM in" protocol=TCP dir=in profile=any localport=5985 remoteip=any localip=any action=deny
-
 # Set Administrator password
 $admin = [adsi]("WinNT://./administrator, user")
 $admin.psbase.invoke("SetPassword", "THIS_IS_NOT_THE_PASSWORD")
 $admin.psbase.CommitChanges()
 
-#wm should go here
+# close the firewall
+netsh advfirewall firewall add rule name="WinRM in" protocol=TCP dir=in profile=any localport=5985 remoteip=any localip=any action=deny
+
+# this will become the watchmaker portion of install
+WATCHMAKER_INSTALL_GOES_HERE
+
+# Set Administrator password - should always go after wm install because username not yet changed
+$admin = [adsi]("WinNT://./WINRM_USER_GOES_HERE, user")
+$admin.psbase.invoke("SetPassword", "THIS_IS_NOT_THE_PASSWORD")
+$admin.psbase.CommitChanges()
 
 # open firewall for winrm
 netsh advfirewall firewall add rule name="WinRM in" protocol=TCP dir=in profile=any localport=5985 remoteip=any localip=any action=allow
@@ -26,14 +32,6 @@ C:\salt\salt-call --local -c C:\Watchmaker\salt\conf lgpo.set_reg_value `
     key='HKLM\SOFTWARE\Policies\Microsoft\Windows\WinRM\Service\AllowUnencryptedTraffic' `
     value='1' `
     vtype='REG_DWORD'
-
-# this will become the watchmaker portion of install
-WATCHMAKER_INSTALL_GOES_HERE
-
-# Set Administrator password - should always go after wm install because username not yet changed
-$admin = [adsi]("WinNT://./WINRM_USER_GOES_HERE, user")
-$admin.psbase.invoke("SetPassword", "THIS_IS_NOT_THE_PASSWORD")
-$admin.psbase.CommitChanges()
 
 Stop-Transcript
 
