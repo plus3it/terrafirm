1ee6c4aa19e92bb77cf27006dbc168dc7e621123
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-19 12:24:38 -0500

    Update terrafirm.tf

--- terrafirm.tf
+++ terrafirm.tf
@@ -211,12 +211,11 @@ data "null_data_source" "spel_instance_amis" {
 
 # bread & butter - this tells TF the provision/create the actual instance
 resource "aws_instance" "spels" {
-  count                        = "1"
-  #count                       = "${length(data.null_data_source.spel_instance_amis.inputs)}"
+  #count                        = "1"
+  count                        = "${length(data.null_data_source.spel_instance_amis.inputs)}"
   ami                          = "${lookup(data.null_data_source.spel_instance_amis.inputs, count.index)}"
   instance_type                = "${var.lx_instance_type}"
   iam_instance_profile         = "${var.instance_profile}"
-  #iam_instance_profile         = ""
   key_name                     = "${aws_key_pair.auth.id}"
   vpc_security_group_ids       = ["${aws_security_group.terrafirm_ssh.id}"]
   user_data                    = "${file("linux/userdata.sh")}"
@@ -259,12 +258,12 @@ data "null_data_source" "windows_instance_amis" {
 
 # bread & butter - this tells TF the provision/create the actual instance
 resource "aws_instance" "windows" {
-  count                        = "0"
-  #count                        = "${length(data.null_data_source.windows_instance_amis.inputs)}"
+  #count                        = "0"
+  count                        = "${length(data.null_data_source.windows_instance_amis.inputs)}"
   ami                          = "${lookup(data.null_data_source.windows_instance_amis.inputs, count.index)}"
   instance_type                = "${var.win_instance_type}"
   key_name                     = "${aws_key_pair.auth.id}"
-  #iam_instance_profile         = "${var.instance_profile}"
+  iam_instance_profile         = "${var.instance_profile}"
   vpc_security_group_ids       = ["${aws_security_group.terrafirm_winrm.id}"]
   user_data                    = "${file("windows/userdata.ps1")}"
   associate_public_ip_address  = "${var.associate_public_ip_address}"  
