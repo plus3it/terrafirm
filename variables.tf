e9bc4d2ef3942c35d57f1b3ee34a0bb837a45a89
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-22 18:39:16 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,8 +14,8 @@ variable "lx_instance_type" {}
 variable "win_instance_type" {}
 variable "subnet_id" {}
 variable "vpc_id" {}
-variable "build_win" {}
-variable "build_lx" {}
+variable "lx_all_one_none" {}
+variable "win_all_one_none" {}
 
 output "amicentos6" {
   value = "${data.aws_ami.centos6.id}"
