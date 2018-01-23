771d80bc7022ef11eb2a8883d01cf577740d48d2
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-22 19:03:22 -0500

    Update terrafirm.tf

--- terrafirm.tf
+++ terrafirm.tf
@@ -217,7 +217,7 @@ data "null_data_source" "spel_instance_amis" {
 resource "aws_instance" "spels" {
   #count                        = "1"
   #count                        = "${length(data.null_data_source.spel_instance_amis.inputs)-var.lx_instance_negation}"
-  count                        = "${element(list(length(data.null_data_source.spel_instance_amis.inputs),"1","0"),var.lx_all_one_none)}"
+  count                        = "${lookup(map("all",length(data.null_data_source.spel_instance_amis.inputs),"one",1,"none",0),var.lx_all_one_none)}"
   ami                          = "${lookup(data.null_data_source.spel_instance_amis.inputs, count.index)}"
   instance_type                = "${var.lx_instance_type}"
   iam_instance_profile         = "${var.instance_profile}"
@@ -266,7 +266,8 @@ data "null_data_source" "windows_instance_amis" {
 resource "aws_instance" "windows" {
   #count                        = "0"
   #count                        = "${length(data.null_data_source.windows_instance_amis.inputs)-var.win_instance_negation}"
-  count                        = "${element(list(length(data.null_data_source.windows_instance_amis.inputs),"1","0"),var.win_all_one_none)}"
+  #count                        = "${element(list(length(data.null_data_source.windows_instance_amis.inputs),"1","0"),var.win_all_one_none)}"
+  count                        = "${lookup(map("all",length(data.null_data_source.windows_instance_amis.inputs),"one",1,"none",0),var.win_all_one_none)}"
   ami                          = "${lookup(data.null_data_source.windows_instance_amis.inputs, count.index)}"
   instance_type                = "${var.win_instance_type}"
   key_name                     = "${aws_key_pair.auth.id}"
