11da83d4e04da76f272caefc63f683854a6c3faf
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-25 18:12:58 -0500

    Update input_variables.tf

--- input_variables.tf
+++ input_variables.tf
@@ -1,25 +1,24 @@
 variable "tfi_region" {}
-variable "tfi_private_key" {}
-variable "tfi_public_key" {}
-variable "tfi_key_pair_name" {}
+variable "tfi_win_security_grp" {}
+variable "tfi_lx_security_grp" {}
 variable "tfi_cb_ip" {}
-variable "tfi_associate_public_ip_address" {}
-variable "tfi_win_security_group" {}
-variable "tfi_lx_security_group" {}
-variable "tfi_instance_profile" {}
-variable "tfi_lx_instance_type" {}
-variable "tfi_win_instance_type" {}
 variable "tfi_subnet_id" {}
 variable "tfi_vpc_id" {}
+variable "tfi_key_pair_name" {}
 variable "tfi_lx_all_one_none" {}
 variable "tfi_win_all_one_none" {}
-
+variable "tfi_private_key" {}
+variable "tfi_public_key" {}
+variable "tfi_rm_pass" {}
+variable "tfi_rm_user" {}
+variable "tfi_ssh_user" {}
+variable "tfi_instance_profile" {}
+variable "tfi_assign_public_ip" {}
+variable "tfi_win_instance_type" {}
+variable "tfi_lx_instance_type" {}
 variable "tfi_repo" {}
 variable "tfi_branch" {}
 variable "tfi_common_args" {}
 variable "tfi_win_args" {}
 variable "tfi_lx_args" {}
-variable "tfi_rm_pass" {}
-variable "tfi_rm_user" {}
-variable "tfi_ssh_user" {}
 
