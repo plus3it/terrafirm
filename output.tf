40801adcb9e9ee725c3117c75f8c9db3d1668cc1
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-25 15:30:58 -0500

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,1 +1,28 @@
 
+output "amicentos6" {
+  value = "${data.aws_ami.centos6.id}"
+}
+
+output "amicentos7" {
+  value = "${data.aws_ami.centos7.id}"
+}
+
+output "amirhel6" {
+  value = "${data.aws_ami.rhel6.id}"
+}
+
+output "amirhel7" {
+  value = "${data.aws_ami.rhel7.id}"
+}
+
+output "ami2016" {
+  value = "${data.aws_ami.windows2016.id}"
+}
+
+output "ami2012" {
+  value = "${data.aws_ami.windows2012.id}"
+}
+
+output "ami2008" {
+  value = "${data.aws_ami.windows2008.id}"
+}
