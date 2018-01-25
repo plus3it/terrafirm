fdd0296d97c91388afc4d1dc9af6b711d671e769
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-25 14:34:49 -0500

    Update install_watchmaker.ps1

--- windows/install_watchmaker.ps1
+++ windows/install_watchmaker.ps1
@@ -1,5 +1,5 @@
-$GitRepo = "${var.tfi_repo}"
-$GitBranch = "${var.tfi_branch}"
+$GitRepo = "${tfi_repo}"
+$GitBranch = "${tfi_branch}"
 
 $BootstrapUrl = "https://raw.githubusercontent.com/plus3it/watchmaker/master/docs/files/bootstrap/watchmaker-bootstrap.ps1"
 $PythonUrl = "https://www.python.org/ftp/python/3.6.3/python-3.6.3-amd64.exe"
@@ -27,4 +27,4 @@ cd watchmaker
 pip install --index-url "$PypiUrl" --editable .
 
 # Run watchmaker
-watchmaker ${var.tfi_common_args} ${var.tfi_win_args}
+watchmaker ${tfi_common_args} ${tfi_win_args}
