80d375c5aa0bff8eab0313f1db90193a45637e9c
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-25 15:32:18 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,27 +1,3 @@
-output "amicentos6" {
-  value = "${data.aws_ami.centos6.id}"
-}
-
-output "amicentos7" {
-  value = "${data.aws_ami.centos7.id}"
-}
-
-output "amirhel6" {
-  value = "${data.aws_ami.rhel6.id}"
-}
-
-output "amirhel7" {
-  value = "${data.aws_ami.rhel7.id}"
-}
-
-output "ami2016" {
-  value = "${data.aws_ami.windows2016.id}"
-}
-
-output "ami2012" {
-  value = "${data.aws_ami.windows2012.id}"
-}
-
-output "ami2008" {
-  value = "${data.aws_ami.windows2008.id}"
+provider "aws" {
+  region     = "${var.tfi_region}"
 }
