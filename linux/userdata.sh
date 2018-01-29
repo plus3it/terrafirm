ea9c56abac0ffd543a9f495afe08c48a7adb3ecc
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-29 11:08:18 -0500

    Update userdata.sh

--- linux/userdata.sh
+++ linux/userdata.sh
@@ -10,8 +10,15 @@ WATCHMAKER_INSTALL_GOES_HERE
 
 #copy files to S3 using AWS CLI
 pip install awscli --upgrade
-export AWS_ACCESS_KEY_ID=${tfi_cli_access_key_id}
-export AWS_SECRET_ACCESS_KEY=${tfi_cli_secret_access_key}
-export AWS_DEFAULT_REGION=${tfi_region}
+export AWS_ACCESS_KEY_ID="${tfi_cli_access_key_id}"
+export AWS_SECRET_ACCESS_KEY="${tfi_cli_secret_access_key}"
+export AWS_DEFAULT_REGION="${tfi_region}"
+
+RAND=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1) ; export RAND
+DIRNAME=$(date +'%Y%m%d_%H%M%S_')$RAND ; export DIRNAME
+
+aws s3 cp ${tfi_lx_userdata_log} s3://terrafirm/$DIRNAME/userdata_install.log
+aws s3 cp /var/log/cloud-init-output.log s3://terrafirm/$DIRNAME/cloud-init-output.log
+aws s3 cp /var/log/watchmaker s3://terrafirm/$DIRNAME/watchmaker/ --recursive
 
 touch /tmp/SETUP_COMPLETE_SIGNAL
