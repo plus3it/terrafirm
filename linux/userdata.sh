35f3c8cefeecba375397a2826b1a261e262c492c
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-29 12:48:13 -0500

    Update userdata.sh

--- linux/userdata.sh
+++ linux/userdata.sh
@@ -5,8 +5,13 @@ exec &> ${tfi_lx_userdata_log}
 yum -y install bc
 
 #sleep 20
+start=`date +%s`
 
 WATCHMAKER_INSTALL_GOES_HERE
+
+end=`date +%s`
+runtime=$((end-start))
+echo "WAM install took $runtime seconds."
 
 #copy files to S3 using AWS CLI
 pip install awscli --upgrade
@@ -15,8 +20,8 @@ export AWS_SECRET_ACCESS_KEY="${tfi_cli_secret_access_key}"
 export AWS_DEFAULT_REGION="${tfi_region}"
 
 RAND=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1) ; export RAND
-DIRNAME=$(date +'%Y%m%d_%H%M%S_')$RAND ; export DIRNAME
 VERSION=$(cat /etc/redhat-release | cut -c1-3)$(cat /etc/redhat-release | sed 's/[^0-9.]*\([0-9.]*\).*/\1/') ; export VERSION
+DIRNAME=$(date +'%Y%m%d_%H%M%S_')$VERSION"_"$RAND ; export DIRNAME
 
 aws s3 cp ${tfi_lx_userdata_log} s3://terrafirm/$DIRNAME/userdata_install.log
 aws s3 cp /var/log/cloud-init-output.log s3://terrafirm/$DIRNAME/cloud-init-output.log
