e5b0a13519ce12ba04ed0fb3f4ad0770fd65e371
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-30 13:46:53 -0500

    Update userdata.sh

--- linux/userdata.sh
+++ linux/userdata.sh
@@ -13,19 +13,6 @@ end=`date +%s`
 runtime=$((end-start))
 echo "WAM install took $runtime seconds."
 
-#echo "AKI: ${tfi_cli_access_key_id}"
-#echo "SAK: ${tfi_cli_secret_access_key}"
-#echo "REG: ${tfi_region}"
-
-#sudo -i
-
-#copy files to S3 using AWS CLI
-#pip install --upgrade pip
-#pip install awscli --upgrade
-#export AWS_ACCESS_KEY_ID="${tfi_cli_access_key_id}"
-#export AWS_SECRET_ACCESS_KEY="${tfi_cli_secret_access_key}"
-#export AWS_DEFAULT_REGION="${tfi_region}"
-
 export S3_TOP_FOLDER=$(date +'%Y%m%d')
 export RAND=$(date +%N | cut -b 1-4) #using nanoseconds, don't use /dev/urandom here because there's not enough entropy yet! will block on RHEL
 export OS_VERSION=$(cat /etc/redhat-release | cut -c1-3)$(cat /etc/redhat-release | sed 's/[^0-9.]*\([0-9.]*\).*/\1/')
