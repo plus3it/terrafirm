4215d5f08190c21070ca4fe576e2143bf33edddb
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-30 13:25:53 -0500

    Update userdata.sh

--- linux/userdata.sh
+++ linux/userdata.sh
@@ -2,7 +2,6 @@
 
 exec &> ${tfi_lx_userdata_log}
 
-whoami
 yum -y install bc
 
 #sleep 20
@@ -10,13 +9,10 @@ start=`date +%s`
 
 WATCHMAKER_INSTALL_GOES_HERE
 
-whoami
 end=`date +%s`
 runtime=$((end-start))
 echo "WAM install took $runtime seconds."
 
-setenforce 0
-echo "no enforcement"
 #echo "AKI: ${tfi_cli_access_key_id}"
 #echo "SAK: ${tfi_cli_secret_access_key}"
 #echo "REG: ${tfi_region}"
@@ -30,22 +26,18 @@ echo "no enforcement"
 #export AWS_SECRET_ACCESS_KEY="${tfi_cli_secret_access_key}"
 #export AWS_DEFAULT_REGION="${tfi_region}"
 
-export TOP_FOLDER=$(date +'%Y%m%d')
-echo "Top folder: $TOP_FOLDER"
-#$(sudo -i cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1) > /tmp/rand
-#export RAND=$(file(/tmp/rand))
-export RAND=abcd
-echo "Rand: $RAND"
-export VERSION=$(cat /etc/redhat-release | cut -c1-3)$(cat /etc/redhat-release | sed 's/[^0-9.]*\([0-9.]*\).*/\1/')
-echo "Version: $VERSION"
-export DIRNAME=$(date +'%Y%m%d_%H%M%S_')$VERSION"_"$RAND
-echo "Dir name: $DIRNAME"
+export S3_TOP_FOLDER=$(date +'%Y%m%d')
+export RAND=$(date +%N | cut-c1-c4) #don't use /dev/urandom here because there's not enough entropy yet! will block on RHEL
+export OS_VERSION=$(cat /etc/redhat-release | cut -c1-3)$(cat /etc/redhat-release | sed 's/[^0-9.]*\([0-9.]*\).*/\1/')
+export S3_FOLDER=$(date +'%Y%m%d_%H%M%S_')$OS_VERSION"_"$RAND
 
-echo "Dir name: $${DIRNAME}"
-echo "Top folder: $${TOP_FOLDER}"
-
-aws s3 cp /tmp/userdata.log "s3://terrafirm/$${TOP_FOLDER}/$${DIRNAME}/userdata.log"
-aws s3 cp /var/log/cloud* "s3://terrafirm/$${TOP_FOLDER}/$${DIRNAME}/cloud-init/"
-aws s3 cp /var/log/watchmaker "s3://terrafirm/$${TOP_FOLDER}/$${DIRNAME}/watchmaker/" --recursive
+aws s3 cp /tmp/userdata.log "s3://terrafirm/$${S3_TOP_FOLDER}/$${S3_FOLDER}/userdata.log"
+FILES=/var/log/cloud*
+for f in $FILES
+do
+  # wildcard with awscli works fine on 7.4 but not on 6.9 for whatever reason so for-looping it
+  aws s3 cp $f "s3://terrafirm/$${S3_TOP_FOLDER}/$${S3_FOLDER}/cloud-init/"
+done
+aws s3 cp /var/log/watchmaker "s3://terrafirm/$${S3_TOP_FOLDER}/$${S3_FOLDER}/watchmaker/" --recursive
 
 touch /tmp/SETUP_COMPLETE_SIGNAL
