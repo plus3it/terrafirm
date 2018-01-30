1a5941b536a9cd664d92c2afe3cf2cc917fd42ac
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-30 17:35:45 -0500

    Update userdata.ps1

--- windows/userdata.ps1
+++ windows/userdata.ps1
@@ -33,6 +33,9 @@ C:\salt\salt-call --local -c C:\Watchmaker\salt\conf lgpo.set_reg_value `
     value='1' `
     vtype='REG_DWORD'
     
+Stop-Transcript
+
+# upload logs to S3 bucket
 $S3_TOP_FOLDER=Get-Date -UFormat "%Y%m%d"
 $RAND=-join ((48..57) + (65..90) + (97..122) | Get-Random -Count 4 | % {[char]$_})
 $OS_VERSION="Win" + (((Get-WmiObject -class Win32_OperatingSystem).Caption) -replace '.+(\d\d)\s(.{2}).+','$1$2')
@@ -41,8 +44,6 @@ $S3_FOLDER=(Get-Date -UFormat "%Y%m%d_%H%M%S_") + $OS_VERSION + "_" + $RAND
 Write-S3Object -BucketName "terrafirm/$S3_TOP_FOLDER/$S3_FOLDER" -File ${tfi_win_userdata_log}
 Write-S3Object -BucketName "terrafirm/$S3_TOP_FOLDER/$S3_FOLDER" -Folder "C:\\Program Files\\Amazon\\Ec2ConfigService\\Logs" -KeyPrefix cloud-init\
 Write-S3Object -BucketName "terrafirm/$S3_TOP_FOLDER/$S3_FOLDER" -Folder "C:\\Watchmaker\\Logs" -KeyPrefix watchmaker\ -SearchPattern *.log
-
-Stop-Transcript
 
 # script will setup winrm and set the timeout
 </powershell>
