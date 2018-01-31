e1ec6e08fa9ac2e5fbb98db53ddfc1250f60d5a9
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-31 13:01:18 -0500

    Update userdata.ps1

--- windows/userdata.ps1
+++ windows/userdata.ps1
@@ -46,6 +46,9 @@ Stop-Transcript
 $S3_TOP_KEYFIX=("${tfi_build_id}" -split "_",2)[0]
 #$RAND=-join ((65..90) + (97..122) | Get-Random -Count 4 | % {[char]$_})
 $OS_VERSION="Win" + (((Get-WmiObject -class Win32_OperatingSystem).Caption) -replace '.+(\d\d)\s(.{2}).+','$1$2')
+If ($OS_VERSION.Substring($OS_VERSION.get_Length()-2) -eq 'Da') {
+  $OS_VERSION -replace ".{2}$"
+}
 $S3_KEYFIX=(Get-Date -UFormat "%H%M%S_") + $OS_VERSION
 
 Write-S3Object -BucketName "${tfi_s3_bucket}/$S3_TOP_KEYFIX/${tfi_build_id}/$S3_KEYFIX" -File ${tfi_win_userdata_log} -ErrorAction SilentlyContinue
