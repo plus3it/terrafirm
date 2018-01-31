7e8a7bf898e3d947b071a598bfb2ac62110461a1
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-31 11:18:51 -0500

    Update userdata.sh

--- linux/userdata.sh
+++ linux/userdata.sh
@@ -18,8 +18,8 @@ export RAND=$(date +%N | cut -b 1-4) #using nanoseconds, don't use /dev/urandom 
 export OS_VERSION=$(cat /etc/redhat-release | cut -c1-3)$(cat /etc/redhat-release | sed 's/[^0-9.]*\([0-9]\.[0-9]\).*/\1/')
 export S3_KEYFIX=$(date +'%Y%m%d_%H%M%S_')$OS_VERSION"_"$RAND
 
-aws s3 cp ${tfi_lx_userdata_log} "s3://${tfi_s3_bucket}/$${S3_TOP_KEYFIX}/$${S3_KEYFIX}/userdata.log" || true
-aws s3 cp /var/log "s3://${tfi_s3_bucket}/$${S3_TOP_KEYFIX}/$${S3_KEYFIX}/cloud-init/" --recursive --exclude "*" --include "cloud*log" || true
-aws s3 cp /var/log/watchmaker "s3://${tfi_s3_bucket}/$${S3_TOP_KEYFIX}/$${S3_KEYFIX}/watchmaker/" --recursive || true
+aws s3 cp ${tfi_lx_userdata_log} "s3://${tfi_s3_bucket}/$${S3_TOP_KEYFIX}/${tfi_build_id}/$${S3_KEYFIX}/userdata.log" || true
+aws s3 cp /var/log "s3://${tfi_s3_bucket}/$${S3_TOP_KEYFIX}/${tfi_build_id}/$${S3_KEYFIX}/cloud-init/" --recursive --exclude "*" --include "cloud*log" || true
+aws s3 cp /var/log/watchmaker "s3://${tfi_s3_bucket}/$${S3_TOP_KEYFIX}/${tfi_build_id}/$${S3_KEYFIX}/watchmaker/" --recursive || true
 
 touch /tmp/SETUP_COMPLETE_SIGNAL
