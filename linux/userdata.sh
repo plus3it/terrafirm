e971b004be3d4193fcc7f683c156be80db594298
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-30 19:02:22 -0500

    Update userdata.sh

--- linux/userdata.sh
+++ linux/userdata.sh
@@ -16,7 +16,7 @@ echo "WAM install took $runtime seconds."
 export S3_TOP_FOLDER=$(date +'%Y%m%d')
 export RAND=$(date +%N | cut -b 1-4) #using nanoseconds, don't use /dev/urandom here because there's not enough entropy yet! will block on RHEL
 export OS_VERSION=$(cat /etc/redhat-release | cut -c1-3)$(cat /etc/redhat-release | sed 's/[^0-9.]*\([0-9]\.[0-9]\).*/\1/')
-export S3_FOLDER=$(date +'%H%M%S_')$OS_VERSION"_"$RAND
+export S3_FOLDER=$(date +'%Y%m%d_%H%M%S_')$OS_VERSION"_"$RAND
 
 aws s3 cp ${tfi_lx_userdata_log} "s3://terrafirm/$${S3_TOP_FOLDER}/$${S3_FOLDER}/userdata.log"
 aws s3 cp /var/log "s3://terrafirm/$${S3_TOP_FOLDER}/$${S3_FOLDER}/cloud-init/" --recursive --exclude "*" --include "cloud*log"
