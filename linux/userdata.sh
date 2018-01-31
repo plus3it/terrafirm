fd47b774f0f95005982cbb7d725767b013766524
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-31 12:40:58 -0500

    Update userdata.sh

--- linux/userdata.sh
+++ linux/userdata.sh
@@ -15,7 +15,7 @@ echo "WAM install took $runtime seconds."
 
 export S3_TOP_KEYFIX=$(echo ${tfi_build_id} | cut -d'_' -f 1)
 export OS_VERSION=$(cat /etc/redhat-release | cut -c1-3)$(cat /etc/redhat-release | sed 's/[^0-9.]*\([0-9]\.[0-9]\).*/\1/')
-export S3_KEYFIX=$(date +'%Y%m%d_%H%M%S_')$OS_VERSION
+export S3_KEYFIX=$(date +'%H%M%S_')$OS_VERSION
 
 aws s3 cp ${tfi_lx_userdata_log} "s3://${tfi_s3_bucket}/$${S3_TOP_KEYFIX}/${tfi_build_id}/$${S3_KEYFIX}/userdata.log" || true
 aws s3 cp /var/log "s3://${tfi_s3_bucket}/$${S3_TOP_KEYFIX}/${tfi_build_id}/$${S3_KEYFIX}/cloud-init/" --recursive --exclude "*" --include "cloud*log" || true
