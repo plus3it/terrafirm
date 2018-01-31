dc9677691e9e42fe090439c1c9879be5691a87d9
YakDriver <31492422+YakDriver@users.noreply.github.com>
2018-01-31 12:05:10 -0500

    Update userdata.ps1

--- windows/userdata.ps1
+++ windows/userdata.ps1
@@ -44,9 +44,9 @@ Stop-Transcript
 
 # upload logs to S3 bucket
 $S3_TOP_KEYFIX=("${tfi_build_id}" -split "_",2)[0]
-$RAND=-join ((65..90) + (97..122) | Get-Random -Count 4 | % {[char]$_})
+#$RAND=-join ((65..90) + (97..122) | Get-Random -Count 4 | % {[char]$_})
 $OS_VERSION="Win" + (((Get-WmiObject -class Win32_OperatingSystem).Caption) -replace '.+(\d\d)\s(.{2}).+','$1$2')
-$S3_KEYFIX=(Get-Date -UFormat "%Y%m%d_%H%M%S_") + $OS_VERSION + "_" + $RAND
+$S3_KEYFIX=(Get-Date -UFormat "%Y%m%d_%H%M%S_") + $OS_VERSION
 
 Write-S3Object -BucketName "${tfi_s3_bucket}/$S3_TOP_KEYFIX/${tfi_build_id}/$S3_KEYFIX" -File ${tfi_win_userdata_log} -ErrorAction SilentlyContinue
 Write-S3Object -BucketName "${tfi_s3_bucket}" -Folder "C:\\Program Files\\Amazon\\Ec2ConfigService\\Logs" -KeyPrefix $S3_TOP_KEYFIX/${tfi_build_id}/$S3_KEYFIX/cloud-init/ -ErrorAction SilentlyContinue
